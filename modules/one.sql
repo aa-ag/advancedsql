a3deab3966ee2dc5cd4441df2de43d4c0b0774c6
aa-ag <aaron.aguerrevere@gmail.com>
2023-05-24 11:47:44 -0500

    Add first stab at second query from class

--- modules/one.sql
+++ modules/one.sql
@@ -1,3 +1,4 @@
+-- 1
 SELECT q.id AS q_id,
     MIN(TIMESTAMP_DIFF(a.creation_date, q.creation_date, SECOND)) as time_to_answer
 FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
@@ -6,3 +7,14 @@ ON q.id = a.parent_id
 WHERE q.creation_date >= '2018-01-01' and q.creation_date < '2018-02-01'
 GROUP BY q_id
 ORDER BY time_to_answer
+
+-- 2
+SELECT q.owner_user_id AS owner_user_id,
+    MIN(q.creation_date) AS q_creation_date,
+    MIN(a.creation_date) AS a_creation_date
+FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
+    ____ `bigquery-public-data.stackoverflow.posts_answers` AS a
+ON q.owner_user_id = a.owner_user_id 
+WHERE q.creation_date >= '2019-01-01' AND q.creation_date < '2019-02-01' 
+    AND a.creation_date >= '2019-01-01' AND a.creation_date < '2019-02-01'
+GROUP BY owner_user_id
