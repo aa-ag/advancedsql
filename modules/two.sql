c96506fc04bd2833d82a487af8815fddb655a24b
aa-ag <aaron.aguerrevere@gmail.com>
2023-05-31 10:23:20 -0500

    Add second example

--- modules/two.sql
+++ modules/two.sql
@@ -1,3 +1,4 @@
+-- First example
 WITH trips_by_day AS
 (
 SELECT DATE(start_date) AS trip_date,
@@ -13,3 +14,23 @@ SELECT *,
             ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
             ) AS cumulative_trips
     FROM trips_by_day
+
+-- Second example
+SELECT bike_number,
+    TIME(start_date) AS trip_time,
+    FIRST_VALUE(start_station_id)
+        OVER (
+            PARTITION BY bike_number
+            ORDER BY start_date
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+            ) AS first_station_id,
+    LAST_VALUE(end_station_id)
+        OVER (
+            PARTITION BY bike_number
+            ORDER BY start_date
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+            ) AS last_station_id,
+    start_station_id,
+    end_station_id
+FROM `bigquery-public-data.san_francisco.bikeshare_trips`
+WHERE DATE(start_date) = '2015-10-25'
