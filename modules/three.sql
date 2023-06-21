7ad9bd33a3ca18d53b83af83aae726e1d57797a8
aa-ag <aaron.aguerrevere@gmail.com>
2023-06-20 20:51:54 -0500

    Add query to rank landing pages by popularity

--- modules/three.sql
+++ modules/three.sql
@@ -1,5 +1,7 @@
-SELECT device.browser AS device_browser,
-    SUM(totals.transactions) as total_transactions
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`
-GROUP BY device_browser
-ORDER BY total_transactions DESC
+SELECT hits.page.pagePath as path,
+    COUNT(hits.page.pagePath) as counts
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`, 
+    UNNEST(hits) as hits
+WHERE hits.type="PAGE" and hits.hitNumber=1
+GROUP BY path
+ORDER BY counts DESC
